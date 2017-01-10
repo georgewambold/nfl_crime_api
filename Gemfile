e4eadfe357ffd762d7ac6705d75e1da12c5f360e
George Wambold <gwambold@Georges-MacBook-Pro.local>
2017-01-10 16:09:55 -0600

    Switch out unit_test for RSpec

--- Gemfile
+++ Gemfile
@@ -26,6 +26,7 @@ gem 'puma', '~> 3.0'
 # gem 'rack-cors'
 
 group :development, :test do
+  gem 'rspec-rails'
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
 end
--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,10 @@ module NflCrimeApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    
+    # Rspec
+    config.generators do |g|
+      g.test_framework :rspec
+    end
   end
 end
