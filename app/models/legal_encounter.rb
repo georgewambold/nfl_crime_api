303ae315eccfa471a5915220f0f730dffaf7d8b7
George Wambold <georgewambold@gmail.com>
2017-01-10 17:59:47 -0600

    Create legal_encounters table and LegalEncounter model

---
+++ app/models/legal_encounter.rb
@@ -0,0 +1,2 @@
+class LegalEncounter < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170110233137) do
+ActiveRecord::Schema.define(version: 20170110235405) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "crime_catagories", force: :cascade do |t|
     t.string   "category"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "legal_encounters", force: :cascade do |t|
+    t.string   "type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
