e27e30a773d4ae921ead4d974929d712fac78a62
George Wambold <gwambold@Georges-MacBook-Pro.local>
2017-01-10 16:46:33 -0600

    Create teams table and Team model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170110221334) do
+ActiveRecord::Schema.define(version: 20170110224253) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 20170110221334) do
   create_table "players", force: :cascade do |t|
     t.string   "first_name", null: false
     t.string   "last_name",  null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "teams", force: :cascade do |t|
+    t.string   "team_code"
+    t.string   "team_name"
+    t.string   "team_city"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
