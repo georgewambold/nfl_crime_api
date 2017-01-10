249569f6a1c4a42b4d5120ec72254b24ab3f041c
George Wambold <georgewambold@gmail.com>
2017-01-10 17:32:48 -0600

    Create crime_catagories table and CrimeCatagory model

---
+++ app/models/crime_catagory.rb
@@ -0,0 +1,2 @@
+class CrimeCatagory < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170110224805) do
+ActiveRecord::Schema.define(version: 20170110233137) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "crime_catagories", force: :cascade do |t|
+    t.string   "category"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "players", force: :cascade do |t|
     t.string   "first_name", null: false
