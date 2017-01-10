69174d50e0054d582af46ba6d78c9252dcbe0808
George Wambold <georgewambold@gmail.com>
2017-01-10 16:49:04 -0600

    Create positions table and Position model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170110224253) do
+ActiveRecord::Schema.define(version: 20170110224805) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,12 @@ ActiveRecord::Schema.define(version: 20170110224253) do
   create_table "players", force: :cascade do |t|
     t.string   "first_name", null: false
     t.string   "last_name",  null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "positions", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
