309a6889566010fb9b299676097a2c46167d7cc3
George Wambold <gwambold@Georges-MacBook-Pro.local>
2017-01-10 16:41:52 -0600

    Create players table and Player model

---
+++ db/migrate/20170110221334_create_players.rb
@@ -0,0 +1,10 @@
+class CreatePlayers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :players do |t|
+      t.string :first_name, null: false
+      t.string :last_name, null: false
+
+      t.timestamps
+    end
+  end
+end
