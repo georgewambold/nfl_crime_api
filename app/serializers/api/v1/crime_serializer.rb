16430898df20cdf6b9b746668a02e008e34ca233
George Wambold <georgewambold@gmail.com>
2017-01-11 15:27:20 -0600

    Write more detailed crime serializer

--- app/serializers/api/v1/crime_serializer.rb
+++ app/serializers/api/v1/crime_serializer.rb
@@ -2,17 +2,51 @@ module API
   module V1
     class CrimeSerializer < ActiveModel::Serializer
       attributes :id, :description, :outcome
-      attribute :date_of_incident
-
-
-      def date_of_incident
-        object.date_of_incident.strftime("%Y-%m-%d")
+      
+      attribute :year do 
+        object.date_of_incident.strftime("%Y") 
+      end
+      
+      attribute :month do 
+        object.date_of_incident.strftime("%m") 
+      end
+      
+      attribute :day do 
+        object.date_of_incident.strftime("%d") 
       end
 
-      belongs_to :player
-      belongs_to :team
-      belongs_to :position
-      has_many :crime_categories
+      attribute :team do 
+        object.team.team_code 
+      end
+
+      attribute :team_name do
+        object.team.team_name 
+      end
+
+      attribute :team_city do 
+        object.team.team_city 
+      end
+
+      attribute :name do 
+        player = object.player
+        "#{player.first_name.titleize} #{player.last_name.titleize}"
+      end
+
+      attribute :player_id do
+        object.player.id
+      end 
+
+      attribute :position do 
+        object.position.name
+      end
+
+      attribute :encounter do
+        object.legal_encounter.encounter_type
+      end
+
+      attribute :category do 
+        object.crime_categories.map(&:category).join(", ")
+      end
     end
   end
 end
