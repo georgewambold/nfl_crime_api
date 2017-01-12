691c88be388464154d6a47e2293a948c7cf00ee3
George Wambold <georgewambold@gmail.com>
2017-01-12 11:49:50 -0600

    Create working API Client for nflarrest

---
+++ lib/external_apis/nfl_arrests/nfl_arrests.rb
@@ -0,0 +1,16 @@
+module External_APIs
+  module NFL_arrests
+    class API_client
+      include HTTParty
+      base_uri 'http://nflarrest.com/api'
+
+      def teams
+        self.class.get("/v1/team/")
+      end
+
+      def arrests_for_team(team_code)
+        self.class.get("/v1/team/arrests/" + team_code.downcase)
+      end
+    end
+  end
+end
