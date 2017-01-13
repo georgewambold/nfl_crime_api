19a75db8a99c5fbd904dbbd564fba7ab5be33cc6
George Wambold <georgewambold@gmail.com>
2017-01-13 10:32:44 -0600

    Start adding eager loading to crime and crime_categories controllers

--- app/controllers/api/v1/crime_categories_controller.rb
+++ app/controllers/api/v1/crime_categories_controller.rb
@@ -2,16 +2,20 @@ module API
   module V1
     class CrimeCategoriesController < ApplicationController
       def show
-        @crime_category = CrimeCategory.find(show_params[:id])
+        @crime_category = crime_category_with_associations.find(show_params[:id])
         render json: @crime_category, serializer: CrimeCategoryShowSerializer
       end
 
       def index
-        @crime_categories = CrimeCategory.all
+        @crime_categories = CrimeCategory.includes(:crimes).all
         render json: @crime_categories, root: 'crime_categories', meta: { page: 10 }, adapter: :json
       end
 
       private
+
+      def crime_category_with_associations
+        CrimeCategory.includes(:crimes, :players, :teams, :positions, :legal_encounters)
+      end
 
       def show_params
         params.permit(:id)
